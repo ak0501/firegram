523df9b6de6435c7bc7719e2ab84740063f608f1
ak <avinashkhanal@gmail.com>
2020-10-20 15:32:39 -0500

    corrected progressbar css

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Title/>
       <UploadForm/>
       <ImageGrid selectedImg={setSelectedImg}/>
-      { selectedImg && <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg}/>}
+      { selectedImg && (<Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg}/>)}
     </div>
   );
 }
--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import UseFirestore from '../hooks/useFirestore';
+import useFirestore from '../hooks/useFirestore';
 import { motion } from 'framer-motion';
 
 const ImageGrid = ({ setSelectedImg }) => {
-  const { docs } = UseFirestore('images');
+  const { docs } = useFirestore('images');
 
   return (
     <div className="img-grid">
       {docs && docs.map(doc => (
         <motion.div className="img-wrap" key={doc.id} 
           layout
-          whileHover={{ opacity: 1 }}
+          whileHover={{ opacity: 1 }}s
           onClick={() => setSelectedImg(doc.url)}
         >
           <motion.img src={doc.url} alt="uploaded pic"
